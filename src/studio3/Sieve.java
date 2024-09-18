c5f6e7e3336580cd27f0efaedd21d89c980d4d9a
staceymuthoka <staceymuthoka@staceys-mbp.dhcp.wustl.edu>
2024-09-18 14:18:18 -0500

    Tried to do it but didn't understand what do actually do

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,17 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Choose a number: ");
+		int n = in.nextInt();
+		int[] nums = {n};
+		
+	
+	}
+
+}
